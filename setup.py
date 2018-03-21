20e5206ced4e6d443f6876a4e806d2270ac40cbe
Payton Garland <payton.r.g@gmail.com>
2018-03-21 11:10:29 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+
+setup(name='cabot_check_prometheus',
+      version='0.1.0',
+      description='A Prometheus plugin for Cabot',
+      author='Widen Enterprises',
+      author_email='',
+      url='http://widen.com',
+      packages=find_packages(),
+    )
