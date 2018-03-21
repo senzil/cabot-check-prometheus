bd94bbe8c0a8f7cea090c369f6e970de3d39d8f3
Payton Garland <payton.r.g@gmail.com>
2018-03-21 13:28:51 -0500

    0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(name='cabot_check_prometheus',
-      version='0.1.0',
+      version='0.1.1',
       description='A Prometheus plugin for Cabot',
       author='Widen Enterprises',
       author_email='',
