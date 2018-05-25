6c52f5eb93dbeb624f765ac75a3e03ac2807cbc1
Payton Garland <payton.r.g@gmail.com>
2018-05-25 13:11:36 -0500

    update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(name='cabot_check_prometheus',
-      version='1.0.0',
+      version='1.0.1',
       description='A Prometheus plugin for Cabot',
       author='Widen Enterprises',
       author_email='pgarland@widen.com',
